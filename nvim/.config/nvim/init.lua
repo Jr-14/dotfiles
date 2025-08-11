e8aa9765f72776d6be81fba0877fbfef9b3b55cf
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-08-11 20:20:23 +1000

    Use gruvbox

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -493,8 +493,9 @@ require('lazy').setup({
   -- { 'rose-pine/neovim',
   --   name = 'rose-pine',
   --   priority = 1000,
+  --   lazy = false,
   --   config = function()
-  --     vim.cmd.colorscheme 'rose-pine'
+  --     vim.cmd.colorscheme('rose-pine')
   --   end,
   -- },
   -- { 'rebelot/kanagawa.nvim',
